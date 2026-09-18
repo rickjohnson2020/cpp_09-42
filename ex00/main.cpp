0d7514c88f7bb65bd18e9231301574c24889d4ed
yano <eriksontokyo2020@gmail.com>
2026-09-18 16:25:11 +0900

    change error message for when the number of arguments is more than 2, add/delete test files

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,8 +3,12 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-	if (argc != 2) {
+	if (argc == 1) {
 		std::cout << "Error: could not open file." << std::endl;
+		return 1;
+	}
+	if (argc > 2) {
+		std::cout << "Error: too many arguments." << std::endl;
 		return 1;
 	}
 
